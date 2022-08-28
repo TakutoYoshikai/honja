2eedbe5419b746479739aba441ca88dd830e438a
lucky <takuto.yoshikai@gmail.com>
2022-08-28 12:24:15 +0900

    [add] app.js

--- makeDictionary.js
+++ makeDictionary.js
@@ -1,24 +1,55 @@
 const fs = require("fs");
 const path = require("path");
-const { parse } = require("csv-parse/sync");
+
+const numberAndNextCharDictionary = require("./languages/dictionaries/number_and_nextchar.dict.js");
+
+const languageDictionary = {};
+const numberDictionary = require("./languages/dictionaries/number.dict.js");
+const integerDictionary = require("./languages/dictionaries/integer.dict.js");
+const otherDictionary = {};
+
+otherDictionary["charToVowel.csv"] = require("./languages/dictionaries/charToVowel.csv.dict.js");
+otherDictionary["float.csv"] = require("./languages/dictionaries/float.csv.dict.js");
+otherDictionary["replacejoshi.csv"] = require("./languages/dictionaries/replacejoshi.csv.dict.js");
+otherDictionary["romajiHira.csv"] = require("./languages/dictionaries/romajiHira.csv.dict.js");
+otherDictionary["charToVowel.csv"] = require("./languages/dictionaries/charToVowel.csv.dict.js");
+
+languageDictionary["Thai"] = require("./languages/dictionaries/Thai.dict.js");
+languageDictionary["Russian"] = require("./languages/dictionaries/Russian.dict.js");
+languageDictionary["Arabic"] = require("./languages/dictionaries/Arabic.dict.js");
+languageDictionary["Korean"] = require("./languages/dictionaries/Korean.dict.js");
+languageDictionary["Hindi"] = require("./languages/dictionaries/Hindi.dict.js");
+languageDictionary["Tibetan"] = require("./languages/dictionaries/Tibetan.dict.js");
+languageDictionary["Hebrew"] = require("./languages/dictionaries/Hebrew.dict.js");
+languageDictionary["Khmer"] = require("./languages/dictionaries/Khmer.dict.js");
+languageDictionary["Amharic"] = require("./languages/dictionaries/Amharic.dict.js");
+languageDictionary["Tamil"] = require("./languages/dictionaries/Tamil.dict.js");
+languageDictionary["Armenian"] = require("./languages/dictionaries/Armenian.dict.js");
+languageDictionary["Burmese"] = require("./languages/dictionaries/Burmese.dict.js");
+languageDictionary["Greek"] = require("./languages/dictionaries/Greek.dict.js");
+languageDictionary["Georgian"] = require("./languages/dictionaries/Georgian.dict.js");
+languageDictionary["Sinhalese"] = require("./languages/dictionaries/Sinhalese.dict.js");
+languageDictionary["Romaji"] = require("./languages/dictionaries/Romaji.dict.js");
+languageDictionary["Hiragana"] = require("./languages/dictionaries/Hiragana.dict.js");
+
 
 function loadLangDictionary(name, includesAdditionalDictionary, differentAtEndOfWord) {
-  return require("./languages/dictionaries/" + name + ".dict.js");
+  return languageDictionary[name];
 }
 
 function loadAllNumberWithNextCharDictionaries(fileName) {
-  return require("./languages/dictionaries/number_and_nextchar.dict.js");
+  return numberAndNextCharDictionary;
 }
 
 function loadNumberDictionary() {
-  return require("./languages/dictionaries/number.dict.js");
+  return numberDictionary;
 }
 
 function loadIntegerDictionary() {
-  return require("./languages/dictionaries/integer.dict.js");
+  return integerDictionary;
 }
 function loadOtherDictionary(fileName) {
-  return require("./languages/dictionaries/" + fileName + ".dict.js");
+  return otherDictionary[fileName];
 }
 
 module.exports = {
